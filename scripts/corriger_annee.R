61c13ac1500740c9e8b93b6071e24c29010bb2c7
Melichic <melina@didactic.ca>
2025-04-19 12:05:55 -0400

    Ajout dans le script corriger_annee d'une ligne pour que si NA ajoutée comme aucun format ne correspond, on puisse savoir dans quel fichier et quelle ligne

--- scripts/corriger_annee.R
+++ scripts/corriger_annee.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(lubridate)
 
 # Fonction pour corriger le format des dates et extraire l'année
-corriger_annee <- function(x) {
+corriger_annee <- function(x, ligne = NULL, fichier = NULL) {
   # Cas 1 : Si la valeur est au format YYYY-MM-DDTHH:MM:SS
   if (grepl("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}", x)) {
     return(year(ymd_hms(x)))  # Extraire l'année avec lubridate
@@ -19,6 +19,11 @@ corriger_annee <- function(x) {
     return(as.numeric(x))  # Retourner directement l'année
   }
   
-  # Si aucun format valide n'est détecté, retourner NA
+  # Si aucun format valide n'est détecté, afficher un message et retourner NA
+  msg <- paste0("Aucun format valide détecté pour la valeur : '", x, "'")
+  if (!is.null(fichier)) msg <- paste0(msg, " dans le fichier ", fichier)
+  if (!is.null(ligne)) msg <- paste0(msg, " (ligne ", ligne, ")")
+  message(msg)
+  
   return(NA)
 }
