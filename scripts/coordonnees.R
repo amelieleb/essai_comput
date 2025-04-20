9e4e441874c4150c1652847ad74a64ce891fb3e1
Melichic <melina@didactic.ca>
2025-04-20 16:03:09 -0400

    Modification pour enlever un bug de création de ligne. Le concept reste le même par contre.

--- scripts/coordonnees.R
+++ scripts/coordonnees.R
@@ -1,24 +1,20 @@
-#Version 2 ----
 library(dplyr)
 library(readr)
 
-# Fonction pour vérifier si les coordonnées sont valide
+# Fonction pour vérifier si les coordonnées sont valides
 verification_coordonnees <- function(folder_path) {
   file_list <- list.files(path = folder_path, pattern = "\\.csv$", full.names = TRUE)
   
-  for(file in file_list){
+  for (file in file_list) {
+    df <- read_csv(file, show_col_types = FALSE)
     
-    #Chargement des fichiers
-    df <- read_csv(file, show_col_types=FALSE)
+    # Ajout de la colonne valid_coords
+    df <- df %>%
+      mutate(valid_coords = lat >= 0 & lat <= 90 & lon <= 0 & lon >= -180)
     
-    #Ajout d'une colonne valid_coords: TRUE si les coordonnées sont valides
-    df <-  df %>%
-      mutate(valid_coords = lat>= 0 & lat <= 90 & lon <= 0 & lon >= -180)
-
-    #Sauvegarde le fichier modifié par dessus l'ancien
-    write.csv(df, file)
+    # Sauvegarde sans ligne de noms
+    write_csv(df, file)
     
-    message("Vérification des coordonnées terminées")
+    message("Vérification des coordonnées terminée dans ", basename(file))
   }
 }
-   
