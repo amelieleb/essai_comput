ca56346a413ed7c31259cee7925fd9a677ed855e
amelieleb <ame.leb02@gmail.com>
2025-04-21 20:46:06 -0400

    Ajout en-tête

--- R/correction_des_noms.R
+++ R/correction_des_noms.R
@@ -1,9 +1,19 @@
+######################################################
+# Ce script corrige les erreures possibles dans les noms de colonnes des fichiers csv présents dans le dossier définit ().
+# Attention, ce script appelle une autre fonction : nom_colonne_correction, dans laquelle se trouve toutes les corrections possibles. 
+# Ainsi le script ci-dessous applique les corrections selon ce qui est observé comme nom de colonne et ce qui est attendu selon les erreurs ex: pommes mais attendu pomme, donc corrige pommes --> pomme
+
+# Créé par Mélina Chicoine
+# Date : Création en mars 2025
+######################################################
+
+# Librairie nécessaire
 library(readr)
 
 # Charger la fonction de correction des noms de colonnes
 source("R/nom_colonne_correction.R")
 
-# Fonction pour appliquer la correction des noms de colonnes à tous les fichiers CSV
+# Fonction pour appliquer la correction des noms de colonnes à tous les fichiers CSV. Applique les corrections selon les erreurs possibles décrites dans le script nom_colonne_correction.R
 correction_des_noms <- function(folder_path) {
   message("Correction des noms de colonnes dans tous les fichiers CSV...")
   file_list <- list.files(path = folder_path, pattern = "\\.csv$", full.names = TRUE)
@@ -15,5 +25,7 @@ correction_des_noms <- function(folder_path) {
     write_csv(df, file)  # Sauvegarder le fichier corrigé
   }
   
+  # Message écrit une fois tout terminé
   message("Correction des noms de colonnes terminée.")
 }
+
